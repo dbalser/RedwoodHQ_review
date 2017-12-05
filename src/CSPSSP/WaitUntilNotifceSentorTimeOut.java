c29c003c0387ea618941be7304a93c5d077d4af2
admin <admin@admin.com>
2017-12-05 01:39:15 -0500

    12-5

--- src/CSPSSP/WaitUntilNotifceSentorTimeOut.java
+++ src/CSPSSP/WaitUntilNotifceSentorTimeOut.java
@@ -1,79 +1,98 @@
 package CSPSSP;
 
-import java.util.*;
+import java.util. * ;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import org.junit.Assert;
 import java.util.concurrent.TimeUnit;
 import actions.selenium.Browser;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.*;
+import org.openqa.selenium. * ;
+
+class WaitUntilNotifceSentorTimeOut {
+	public void run(HashMap < String, Object > params)throws Exception {
+	
+		long seconds = 0;
+		long minutes = 0;
+		long maxSeconds = 300;
+		String status;
+		String sentAt;
+
+		String notificationName = Browser.Driver.findElement(By.xpath("//div[@data-mgcompnamevalue='NameGridCol']")).getText();
+		String[]name = notificationName.split("-");
+		String notificationDate = name[9];
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-M-d-HH:mm:ss");
+//		Date scheduledTime = sdf.parse(notificationDate);
+		long scheduledTime = sdf.parse(notificationDate).getTime()/1000;
+		long lastPermissibleSendTime = scheduledTime + maxSeconds;
+		
+		TimeZone tz = TimeZone.getDefault();  
+		Calendar cal = GregorianCalendar.getInstance(tz);
+		int offsetInSeconds = tz.getOffset(cal.getTimeInMillis()) / 1000;
+System.out.println("offsetInSeconds=" + offsetInSeconds);
+System.out.println("lastPermissibleSendTime=" + lastPermissibleSendTime);
+System.out.println("scheduledTime=" + scheduledTime);
+System.out.println("notificationDate=" + notificationDate);
 
 
-class WaitUntilNotifceSentorTimeOut{
-    public void run(HashMap<String, Object> params) throws Exception{
-/*
-      long seconds = 0;
-      long minutes = 0;
-      long maxSeconds = 300;
-      long timeNow;
 
-      while (true) {
-        String status;
-        String sentAt;
-        //String scheduledTime;
+		while (true) {
+			//String scheduledTime;
+			long timeNow = System.currentTimeMillis() % 1000;
+System.out.println("timeNow =" + timeNow);
+			status = Browser.Driver.findElement(By.xpath("//div[@data-mgcompnamevalue='StatusGridCol']")).getText();
+System.out.println("status=" + status);
+			if (status.equalsIgnoreCase("Failed")) {
+				Assert.fail("Notification failed");
+System.out.println("Inside Fail"			);
+			}
+			if (status.equalsIgnoreCase("SCHEDULED")) {
+System.out.println("inside Scheduled");
+//				String scheduledEle = Browser.Driver.findElement(By.xpath("//div[@data-mgcompnamevalue='ScheduledTimeGridCol']")).getText();
+//				while (!scheduledEle.isEmpty()) {}
+				if (timeNow > lastPermissibleSendTime) {
+					Assert.fail("Notification Timed out before being sent.");
+				}
+			}
 
-        status = Browser.Driver.findElement(By.xpath("//div[@data-mgcompnamevalue='StatusGridCol']")).getText();
 
-        if(status.equalsIgnoreCase("Failed")){
-          Assert.fail("Notification failed");
-        }
 
-        String notificationName = Browser.Driver.findElement(By.xpath("//div[@data-mgcompnamevalue='NameGridCol']")).getText();
-        String[] name = notificationName.split("-");
-        String notificationDate = name[9];
-        Date schedule = sdf.parse(notificationDate);
-        
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-M-d-HH:mm:ss");
+//			Calendar calender = new GregorianCalendar();
+//			calender.setTime(scheduledTime);
+//			calender.add(Calendar.HOUR, 5);
+//			Date date = calender.getTime();
 
-        sentAt = Browser.Driver.findElement(By.xpath("//td[@data-mgcompname='UpdatedBycol']/following-sibling::td[@data-mgcompname='SentAtGridCol']/div").getText(); 
-        Date sentDate = sdf.parse(sentAt);
+//			long timeDiff = sentDate.getTime() - date.getTime();
+//			seconds = TimeUnit.MILLISECONDS.toSeconds(timeDiff);
 
-        Calendar calender = new GregorianCalendar();
-        calender.setTime(schedule);
-        calender.add(Calendar.HOUR, 5);
-        Date date = calender.getTime();
+			if (status.equalsIgnoreCase("Completed")) {
+System.out.println("InsideCompleted");
+			sentAt = Browser.Driver.findElement(By.xpath(
+			"//td[@data-mgcompname='UpdatedBycol']/following-sibling::td[@data-mgcompname='SentAtGridCol']/div")).getText();
+System.out.println("sentAt=" + sentAt);
+//				Date sentDate = sdf.parse(sentAt);
+				long sentTime = sdf.parse(sentAt).getTime()/1000 - offsetInSeconds;
+System.out.println("sendTime="+sentTime);
 
-        long timeDiff = sentDate.getTime() - date.getTime();
-        seconds = TimeUnit.MILLISECONDS.toSeconds(timeDiff);
-        timeNow = System.currentTimeMillis();
-           
+				if (sentTime > lastPermissibleSendTime) {
+					Assert.fail("The Notification was sent but the delay in sending was more than permitted.");
+				}
+				return;
+			}
 
-        if(status.equalsIgnoreCase("Completed")){
-          if(seconds > maxSeconds){
-            Assert.fail("Notification failed and the time difference is "+seconds+" seconds.");
-          }
-          return
-        }
-        
-        if(status.equalsIgnoreCase("SCHEDULED")){
-          String scheduledEle = Browser.Driver.findElement(By.xpath("//div[@data-mgcompnamevalue='ScheduledTimeGridCol']")).getText();
-          while(!scheduledEle.isEmpty()){
-          }
-          if(seconds > 300){
-              Assert.fail("Notification failed, the time difference is "+seconds+" seconds.");
-          }
-        }
-        
-        Thread.sleep(15000);
-        WebElement refreshButton = Browser.Driver.findElement(By.xpath("//div[@data-mgcompname='refreshButton']//button"));
-        refreshButton.click();         
-      } */ 
-    }
+
+			Thread.sleep(15000);
+			WebElement refreshButton = Browser.Driver.findElement(By.xpath("//div[@data-mgcompname='refreshButton']//button"));
+			refreshButton.click();
+		}
+	}
 }
-
-
-
-
-
-
+		
+		
+		
+		
+		
+		
+		
+		
+		
