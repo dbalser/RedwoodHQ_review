d073c62edf8a1e95b0d4f1510f308f16baf9b189
jsullivan <jonathan.sullivan@infor.com>
2017-12-11 20:53:03 -0500

    Update CSPVerifyAgentIsLive and CSPVerifyAgentIsRunning.

--- src/CSP-AF/CSPAgents/CSPUtilityServerGroups/CSPVerifyAgentIsLive.cs
+++ src/CSP-AF/CSPAgents/CSPUtilityServerGroups/CSPVerifyAgentIsLive.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 
 using CloudSuitePortal;
+using CloudSuitePortal.Views;
 using CloudSuitePortal.Models;
 using CloudSuitePortal.Services;
 using CloudSuitePortal.Controllers;
@@ -12,9 +14,11 @@ namespace actions.CSP.CSPAgents.CSPUtilityServerGroups
     {
         public void run(Dictionary<string, object> Params)
         {
-            var guid = (string)Params["GUID"];
             var navigationController = CSPBrowser.Application.WindowController.NavigationController;
             var controller = (CSPUtilityServerGroupsController)navigationController.VisibleViewController;
+            
+            var row = controller.Notebook.AgentsNotebookPage.Table.Rows.First();
+            var guid = row.ValueForColumn(CSPAgentsNotebookPage.GUIDColumnId);
             controller.VerifyAgentHeartBeatIsLive(guid);
         }
     }
