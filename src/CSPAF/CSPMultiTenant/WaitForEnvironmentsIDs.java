3a06952da7f7741f949a102de313947e9e83f233
admin <admin@admin.com>
2018-01-23 13:03:27 -0500

    Push

--- src/CSPAF/CSPMultiTenant/WaitForEnvironmentsIDs.java
+++ src/CSPAF/CSPMultiTenant/WaitForEnvironmentsIDs.java
@@ -10,9 +10,10 @@ class WaitForEnvironmentIDs
 {
     public void run(HashMap<String, Object> params)
     {
+        String productName = params.get("Product Name").toString();
         int timeout = Integer.parseInt(params.get("Timeout").toString());
         
-        CSPSuitesController controller=(CSPSuitesController)CSPApplication.getWindowController().getNavigationController().getVisibleViewController();
-        controller.getDetailsNotebookPage().waitForEnvironmentIDs(timeout);
+        CSPSuitesController controller = (CSPSuitesController)CSPApplication.getWindowController().getNavigationController().getVisibleViewController();
+        controller.getDetailsNotebookPage().waitForEnvironmentID(productName, timeout);
     }
 }
