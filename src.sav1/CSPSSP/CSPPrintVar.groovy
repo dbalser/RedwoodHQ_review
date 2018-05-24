318700da94024c5fa85ae4f7b073c21bcde82938
admin <admin@admin.com>
2018-05-24 23:14:51 +0000

    Remove src.sav1

--- src.sav1/CSPSSP/CSPPrintVar.groovy
+++
@@ -1,9 +0,0 @@
-package CSPSSP;
-
-import java.util.*;
-
-class CSPPrintVar{
-    public void run(def params){
-       System.out.println(params.get("value"));
-    }
-}
--- src.sav1/actions/selenium/CSPFail.groovy
+++
@@ -1,8 +0,0 @@
-package actions.selenium;
-
-
-class CSPFail{
-    public void run(def params){
-        assert false
-    }
-}
